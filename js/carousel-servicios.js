febceade80690d197f7401cfb14fd910eb3c879c
Js-focus <juanca943005>
2022-02-20 13:00:48 -0600

    Cambios en querys

--- js/carousel-servicios.js
+++ js/carousel-servicios.js
@@ -10,15 +10,15 @@ window.addEventListener("load", () =>{
     responsive: [
         {
           // screens greater than >= 775px
-          breakpoint: 480,
+          breakpoint: 768,
           settings: {
             // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 1,
-            slidesToScroll: 1,
+            slidesToShow: 2,
+            slidesToScroll: 2,
           }
         },{
             // screens greater than >= 1024px
-            breakpoint: 800,
+            breakpoint: 1024,
             settings: {
               slidesToShow: 3,
               slidesToScroll: 3,
